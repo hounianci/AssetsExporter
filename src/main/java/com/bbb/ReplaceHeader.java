40e349c2359c105f5da95de54ef4ab809e06b036
hounianci <762558614@qq.com>
2021-03-27 11:38:15 +0800

    qs

--- src/main/java/com/bbb/ReplaceHeader.java
+++ src/main/java/com/bbb/ReplaceHeader.java
@@ -10,76 +10,90 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
 public class ReplaceHeader {
-    public static void main(String[] args) {
+
+    public static void processFile(File file, String outputPath){
+        if(!file.getName().endsWith("bundle")){
+            return;
+        }
         ByteArray header = readHeader();
-        File splitDir = new File("bbb/replaceSrc");
-        StringBuilder errorFile = new StringBuilder();
-        int successSize = 0;
-        int failSize = 0;
-        for(File splitSubDir : splitDir.listFiles()){
-            File outputDir = new File("bbb/output/"+splitSubDir.getName());
-            outputDir.mkdir();
-            for(File file : splitSubDir.listFiles()){
-                byte[] bb = new byte[1024*1024*10];
-                int len = 0;
-                try(FileInputStream is = new FileInputStream(file)){
-                    is.skip(0x32);
-                    ByteArray zipData = new ByteArray();
-                    byte[] zipB = new byte[1];
-                    byte[][] zipEnd = {{(byte) 0xf0, (byte)0xe0}, {0x1,0x0,0x14,}};
-                    int[] zipMatchIdx = new int[zipEnd.length];
-                    int zipEndMatchIdx = 0;
-                    a:while(is.read(zipB)!=-1){
-                        if(zipData.getDataLen() > 0x30){
-                            boolean zipEndMatch = false;
-                            for(int i=0; i<zipEnd[zipEndMatchIdx].length; i++){
-                                if(zipB[0]==zipEnd[zipEndMatchIdx][i]){
-                                    zipEndMatch = true;
-                                    zipMatchIdx[zipEndMatchIdx] = i;
-                                    break ;
-                                }
-                            }
-                            if(zipEndMatch){
-                                zipEndMatchIdx++;
-                                if(zipEndMatchIdx==zipEnd.length){
-                                    break a;
-                                }
-                            }else{
-                                if(zipEndMatchIdx==1){
-                                    zipData.addData(zipEnd[0][zipMatchIdx[0]]);
-                                }
-                                zipEndMatchIdx = 0;
-                                zipData.addData(zipB[0]);
-                            }
-                        }else{
-                            zipData.addData(zipB[0]);
+        byte[] bb = new byte[1024*1024*10];
+        int len = 0;
+        try(FileInputStream is = new FileInputStream(file)){
+            is.skip(0x36);
+            ByteArray zipData = new ByteArray();
+            byte[] zipB = new byte[1];
+            byte[][] zipEnd = {{(byte) 0xf0, }, {0x1b,0x01}};
+            int[] zipMatchIdx = new int[zipEnd.length];
+            int zipEndMatchIdx = 0;
+            a:while(is.read(zipB)!=-1){
+                if(zipData.getDataLen() > 0x30){
+                    boolean zipEndMatch = false;
+                    for(int i=0; i<zipEnd[zipEndMatchIdx].length; i++){
+                        if(zipB[0]==zipEnd[zipEndMatchIdx][i]){
+                            zipEndMatch = true;
+                            zipMatchIdx[zipEndMatchIdx] = i;
+                            break ;
                         }
                     }
-                    if(zipData.getDataLen()>300){
-                        continue;
-                    }
-                    header.setLong(0x1e, file.length(), true);
-                    header.setInt(0x26, zipData.getDataLen(), true);
-                    header.setInt(0x2a, calcUncompressDataSize(zipData), true);
-                    try(FileOutputStream os = new FileOutputStream(new File("bbb/output/"+splitSubDir.getName()+"/"+file.getName()+".output"))){
-                        os.write(header.getData());
-                        os.write(zipData.getData());
-                        os.write(zipEnd[0][zipMatchIdx[0]]);
-                        os.write(zipEnd[1][zipMatchIdx[1]]);
-                        while ((len=is.read(bb))!=-1){
-                            os.write(bb, 0, len);
+                    if(zipEndMatch){
+                        zipEndMatchIdx++;
+                        if(zipEndMatchIdx==zipEnd.length){
+                            break a;
                         }
+                    }else{
+                        if(zipEndMatchIdx==1){
+                            zipData.addData(zipEnd[0][zipMatchIdx[0]]);
+                        }
+                        zipEndMatchIdx = 0;
+                        zipData.addData(zipB[0]);
                     }
-                    successSize++;
-                }catch (Exception e){
-                    errorFile.append(file.getName()+", "+file.length()+", "+splitSubDir.getName().length()+"\n");
-                    System.out.println(file.getName());
-                    System.out.println(e.getMessage());
-                    System.out.println("----");
-                    failSize++;
+                }else{
+                    zipData.addData(zipB[0]);
                 }
             }
+            if(zipData.getDataLen()>300){
+                return;
+            }
+            header.setLong(0x1e, file.length(), true);
+            header.setInt(0x26, zipData.getDataLen(), true);
+            header.setInt(0x2a, calcUncompressDataSize(zipData), true);
+            try(FileOutputStream os = new FileOutputStream(new File(outputPath+"/"+file.getName()+".output"))){
+                os.write(header.getData());
+                os.write(zipData.getData());
+                os.write(zipEnd[0][zipMatchIdx[0]]);
+                os.write(zipEnd[1][zipMatchIdx[1]]);
+                while ((len=is.read(bb))!=-1){
+                    os.write(bb, 0, len);
+                }
+            }
+        }catch (Exception e){
+            System.out.println(file.getName());
+            System.out.println(e.getMessage());
+            System.out.println("----");
         }
+    }
+
+    public static void processDir(File dir, String path){
+        if(dir.getName().startsWith("_")){
+            return;
+        }
+        File outputDir = new File(path+"/"+dir.getName());
+        outputDir.mkdirs();
+        for(File splitSubDir : dir.listFiles()){
+            if(splitSubDir.isDirectory()){
+                processDir(splitSubDir, path+dir.getName());
+            }else{
+                processFile(splitSubDir, path+"/"+dir.getName());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        File splitDir = new File("bbb/replaceSrc");
+        StringBuilder errorFile = new StringBuilder();
+        processDir(splitDir,"bbb/output/");
+        int successSize = 0;
+        int failSize = 0;
         try(FileOutputStream os = new FileOutputStream(new File("bbb/output/error.txt"))){
             os.write(("success:"+successSize+"\n").getBytes());
             os.write(("fail:"+failSize+"\n").getBytes());
